1d63a20994f88baa594e56a3b2dc37c300db7377
lysabacate <133353329+lysabacate@users.noreply.github.com>
2025-11-05 13:08:24 +0000

    criando pastas e adicionando coisas base

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getMensagens, criarMensagem } from '@/services/mensagensService.js'
+import MensagemCard from '@/components/MensagemCard.vue'
+import MensagemForm from '@/components/MensagemForm.vue'
+
+</script>
